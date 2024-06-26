31b357b832dd076d8f8cc5bfdfb1029648163a1b
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2024-06-26 09:45:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const yearDigits = document.querySelectorAll('.year-digits');
 
-for (let num = 1; num <= 9; num++) {
-  yearDigits.innerHTML = '';
-  
-  yearDigits.forEach(cell => {
+yearDigits.forEach(cell => {
+  cell.innerHTML = ''; // Ensure the container is empty
+  for (let num = 0; num <= 9; num++) {
     const digit = document.createElement('div');
     digit.innerHTML = `<p>${num}</p>`;
     cell.appendChild(digit);
-  });
-}
+  }
+});
 
 const up = document.querySelectorAll('.up');
 const down = document.querySelectorAll('.down');
 
 up.forEach(upBtn => {
-  
   upBtn.addEventListener('click', () => {
     const year = upBtn.parentElement.querySelector('.year-digits');
     year.scrollBy({
@@ -22,11 +20,9 @@ up.forEach(upBtn => {
       behavior: 'smooth'
     });
   });
-  
 });
 
 down.forEach(downBtn => {
-  
   downBtn.addEventListener('click', () => {
     const year = downBtn.parentElement.querySelector('.year-digits');
     year.scrollBy({
@@ -34,5 +30,4 @@ down.forEach(downBtn => {
       behavior: 'smooth'
     });
   });
-  
-})
+});
